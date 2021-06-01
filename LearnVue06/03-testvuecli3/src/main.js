67c1f1901ce1068bca8ed9787ea17fc986298c7d
cxy593653124 <593653124@qq.com>
2021-06-01 15:14:14 +0800

    main

--- LearnVue06/03-testvuecli3/src/main.js
+++ LearnVue06/03-testvuecli3/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-// 提示信息
+// build的提示信息
 Vue.config.productionTip = false
+
+
+// .mount('#app')与el:'#app'一样
+// vue内部会将el:'#app'转为.mount('#app')
 
 new Vue({
   render: h => h(App),
