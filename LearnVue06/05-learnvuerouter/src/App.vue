739ea433785dbb6630050e2f13707556597390de
cxy593653124 <593653124@qq.com>
2021-06-02 16:17:51 +0800

    21.06.02

---
+++ LearnVue06/05-learnvuerouter/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <!-- <router-link to="/home" tag="button" replace active-class="active">首页</router-link>
+    <router-link to="/about" tag="div" replace>关于</router-link> -->
+		
+		
+		<router-link to="/home" tag="button" replace active-class="active">首页</router-link>
+		<router-link to="/about" tag="div" replace>关于</router-link>
+		<router-view></router-view>
+		
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.router-link-active{
+	color:red
+}
+</style>
---
+++ LearnVue06/05-learnvuerouter/src/router/index.js
@@ -0,0 +1,34 @@
+// 配置路由相关信息
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+import Home from '../components/Home.vue'
+import About from '../components/About.vue'
+
+
+// 1.通过vue.use(插件)来安装插件
+Vue.use(VueRouter)
+
+// 2.创建路由对象，类似new vue({})
+const routes = [
+  {
+    path:'/',
+		rediect:'/Home',//重定向
+  	component:Home
+  },
+	{
+    path:'/home',
+		component:Home
+  },
+	{
+	  path:'/about',
+		component:About
+	},
+]
+const router = new VueRouter({
+  routes,
+	mode:'history',//路径模式；可以由hash改成history
+	linkActiveClass:'active'//活跃时赋予的class名
+})
+
+// 3.将router对象传入到Vue实例中
+export default router
